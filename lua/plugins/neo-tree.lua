54df83e72c3d5183af6d7b2f7124f1a7ca03c137
tien-ce <tien.nguyen11102004@hcmut.edu.vn>
2026-09-21 20:41:20 +0700

    update neo-tree focus on current open file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,8 +15,14 @@ local neotree =
     -- 2. Set up global key maps
     vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { desc = "Toogle Neo-tree", silent = true }) -- Ctrl + N for toggle neo tree
   end,
-  opts = 
-  {
-  }
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true, -- Automatically find and focus the file in the active buffer whenever the current file changes
+        leave_dirs_open = false, -- Close auto-expanded directories when navigating to a different file
+      },
+      use_libuv_file_watcher = true, -- Automatically refresh neo-tree when files are created, deleted, or renamed externally
+    },
+  },
 }
 return neotree
